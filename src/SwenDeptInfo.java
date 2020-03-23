2b133767c1c49d90b12a52869e12d11d224bf7ef
Stephen Cook <31995104+sjc5897@users.noreply.github.com>
2020-03-22 23:00:59 -0400

    Killme

--- src/SwenDeptInfo.java
+++ src/SwenDeptInfo.java
@@ -24,72 +24,49 @@ public class SwenDeptInfo{
 	public void setUp() throws Exception {
 		System.setProperty("webdriver.chrome.driver", "webdrivers/chromedriver"); // can be replaced with Firefox driver
 		driver = new ChromeDriver(); // can be replaced with Firefox
-		baseUrl = "https://www.rit.edu"; // TARGET URL
+		baseUrl = "http://www.rit.edu"; // TARGET URL
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
 	@Test
-	public void testSwen_352() throws Exception {
+	public void testSwenDeptInfo()throws Exception {
 		WebDriverWait wait = new WebDriverWait(driver, 20);
 		//Goes to RIT website
 		driver.get(baseUrl);
-		
-		//Clicks the Academics nav bar
-		driver.findElement(By.id("main-nav--link--academics")).click();
-		
-		//Waits for nav options to be visible
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"block-rit-bootstrap-subtheme-rit-main-menu\"]/ul/li[2]/div")));
-		
-		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("")));
-		//driver.findElement(By.xpath("")).click();
-		
-		//Clicks "Colleges"
-		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\\\"block-rit-bootstrap-subtheme-rit-main-menu\\\"]/ul/li[2]/div/div/ul[1]/li[5]/a")));
+		//Click academics
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='main-nav--link--academics']")));
+		driver.findElement(By.xpath("//*[@id='main-nav--link--academics']")).click();
+		//click colleges
+
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='block-rit-bootstrap-subtheme-rit-main-menu']/ul/li[2]/div/div/ul[1]/li[5]/a")));
 		driver.findElement(By.xpath("//*[@id=\"block-rit-bootstrap-subtheme-rit-main-menu\"]/ul/li[2]/div/div/ul[1]/li[5]/a")).click();
-		
-		//Clicks to Golisano Page
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"block-rit-bootstrap-subtheme-content\"]/div/div[3]/div[3]/div/div/div/div/div/div/div/div/ul/li[3]/a")));
-		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"block-rit-bootstrap-subtheme-content\"]/div/div[3]/div[3]/div/div/div/div/div/div/div/div/ul/li[3]/a")));
-		driver.findElement(By.xpath("//*[@id=\"block-rit-bootstrap-subtheme-content\"]/div/div[3]/div[3]/div/div/div/div/div/div/div/div/ul/li[3]/a")).click();
-		
-		//Clicks to contact button
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"block-rit-bootstrap-subtheme-rit-main-menu\"]/ul/li[7]/a")));
-		driver.findElement(By.xpath("//*[@id=\"block-rit-bootstrap-subtheme-rit-main-menu\"]/ul/li[7]/a")).click();
-		
-		//CLicks SE Department tab
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("\"//*[@id=\\\"card-header-4291\\\"]/h5/a\"")));
-		driver.findElement(By.xpath("\"//*[@id=\\\"card-header-4291\\\"]/h5/a\"")).click();
-		
-		//Wait for dropdown to be visible
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"card-collapse-4291\"]/div/div/article")));
-		
-		
-		
-		
-		//TODO: Extract data and print to console, remove code below
+		//click golisano
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='block-rit-bootstrap-subtheme-content']/div/div[3]/div[3]/div/div/div/div/div/div/div/div/ul/li[3]/a")));
 
-		
-		
-		
-		//get result(s)
-		List<WebElement> myElements = driver.findElements(By.cssSelector("field-content row pb-4 px-3 mb-4 border-bottom border-neutral-cool-gray"));
+		driver.findElement(By.xpath("//*[@id='block-rit-bootstrap-subtheme-content']/div/div[3]/div[3]/div/div/div/div/div/div/div/div/ul/li[3]/a")).click();
+		//click contact
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='block-rit-bootstrap-subtheme-rit-main-menu']/ul/li[7]/a")));
 
-		//Print relevant values
-		for (WebElement e : myElements) {
-			System.out.println();
-			String a = e.getText();
-			String[] array = a.split("\n");
-			String[] line = Arrays.copyOfRange(array,7,11);
-			for (String answer : line ) {
-				System.out.print(answer + " ");
-			}
-		}
+		driver.findElement(By.xpath("//*[@id='block-rit-bootstrap-subtheme-rit-main-menu']/ul/li[7]/a")).click();
+
+		//expand SE contact
+		driver.findElement(By.xpath("//*[@id='card-header-4291']/h5/a")).click();
+
+		//print the information
+		System.out.println(driver.findElement(By.xpath("//*[@id='card-collapse-4291']/div/div/article/div/div[2]/div[1]/a")).getText());
+		System.out.println(driver.findElement(By.xpath("//*[@id=\"card-collapse-4291\"]/div/div/article/div/div[3]/div[1]/a")).getText());
+		System.out.println(driver.findElement(By.xpath("///*[@id=\"card-collapse-4291\"]/div/div/article/div/div[2]/div[2]")).getText());
+		System.out.println(driver.findElement(By.xpath("//*[@id=\"card-collapse-4291\"]/div/div/article/div/div[2]/div[3]")).getText());
+		System.out.println(driver.findElement(By.xpath("//*[@id=\"card-collapse-4291\"]/div/div/article/div/div[2]/div[4]"")).getText());
+
+
 	}
+
 
 	@After
 	public void tearDown() throws Exception {
-		driver.quit();
-		//System.out.println();
+		//driver.quit();
+		System.out.println();
 	}
 
 }
